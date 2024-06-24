8f1f7d1b1f54a4f69af5067cff97d3940f2f347a
IteshkumarS <156979957+IteshkumarS@users.noreply.github.com>
2024-06-24 14:16:38 +0530

    First Commit
    Started something amazing

---
+++ client/script.js
@@ -0,0 +1,11 @@
+  import bot from  './assets/bog.svg';
+  import user from  './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatcontainer = document.querySelector('#chat_container');
+
+let loadinterval;
+
+function loader(element) {
+ '...'
+}
